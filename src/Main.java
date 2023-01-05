00548169eef33f30c1ba136b6bec8de1f4fe39d5
BT_3W2_04 <vincentl105@nycstudents.net>
2023-01-05 09:28:51 -0500

    Part 1 Number 4

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 public class Main {
     public static void main (String [] args) {
-        String[] animals = new String[4];
+        double[] array1 = {7.5, 10.0};
+        double[] array2 = {8.2, 14.8};
+        double[] array3 = new double[2];
 
-        for (int i = 0; i < animals.length; i ++){
-            System.out.println(animals[i]);
-        }
+        array3[0] = array1[0] + array2[0];
+        array3[1] = array1[1] * array2[1];
 
-        animals[0] = "dog";
-        animals[1] = "camel";
-        animals[2] = "aardvark";
-        animals[3] = "bunny";
-
-        for (int i = 0; i < animals.length; i ++){
-            System.out.println(animals[i]);
-        }
+        System.out.println(array3[0]);
+        System.out.println(array3[1]);
     }
 }
